a116b321f4c1d4214d6c69b603b47dbc3905dc17
thiago bueno garcia <thiagobuenogarcia@thiagos-Mini.home>
2023-09-15 20:23:30 -0400

    24th commit to resume screen for mobile view

--- src/components/pdf.tsx
+++ src/components/pdf.tsx
@@ -5,10 +5,10 @@ export const Pdf = () => {
   
     return (
         <div className="bg-purple-300 min-h-screen  p-0 justify-between items-center flex flex-col">
-            <div>
+            <div className="pt-3 sm:pt-1">
             <NavigationBar />
             </div>
-            <div className="flex-1 justify-center items-center mt-10 sm:mb-10">
+            <div className="flex-1 justify-center items-center mt-40 sm:mb-10">
             <div className="rounded-3xl shadow-2xl  shadow-neonBlue mx-10">
                 <img src={resume} alt="resume" 
                 className="object-contain h-full w-full rounded-lg"
